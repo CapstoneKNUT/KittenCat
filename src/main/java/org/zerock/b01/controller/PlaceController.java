652672ab7177f1765621514f7baa48fd7f689560
djatjs <umsunil2@naver.com>
2024-10-01 17:39:37 +0900

    선일 천재

--- src/main/java/org/zerock/b01/controller/PlaceController.java
+++ src/main/java/org/zerock/b01/controller/PlaceController.java
@@ -14,7 +14,7 @@ import org.zerock.b01.service.PlaceService;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/place")
+@RequestMapping("/api/placee")
 @Log4j2
 @RequiredArgsConstructor
 public class PlaceController {
